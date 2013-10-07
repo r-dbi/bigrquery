b36220d5f8376b9caf5871412aafba8daf5c7009
hadley <h.wickham@gmail.com>
2013-10-07 10:26:52 -0500

    Basic support for uploads

--- R/request.r
+++ R/request.r
@@ -1,4 +1,5 @@
 base_url <- "https://www.googleapis.com/bigquery/v2/"
+upload_url <- "https://www.googleapis.com/upload/bigquery/v2/"
 
 #' @importFrom httr GET config
 bq_get <- function(url, config = NULL, ..., sig = get_sig()) {
@@ -23,6 +24,21 @@ bq_post <- function(url, body, config = NULL, ..., sig = get_sig()) {
   process_request(req)
 }
 
+#' @importFrom httr POST add_headers config
+#' @importFrom RJSONIO toJSON
+bq_upload <- function(url, parts, config = NULL, ..., sig = get_sig()) {
+  if (is.null(config)) {
+    config <- config()
+  }
+  
+  config <- c(config, sig)
+  
+  url <- paste0(upload_url, url)
+  req <- POST_multipart_related(url, config, parts = parts, ...)
+  process_request(req)
+}
+
+
 #' @importFrom httr http_status content parse_media
 process_request <- function(req) {
   if (http_status(req)$category == "success") {
@@ -38,3 +54,41 @@ process_request <- function(req) {
     stop("HTTP error [", req$status, "] ", out, call. = FALSE)
   }
 }
+
+# Multipart/related ------------------------------------------------------------
+
+
+# http://www.w3.org/Protocols/rfc1341/7_2_Multipart.html
+POST_multipart_related <- function(url, config = NULL, parts = NULL, ...,
+                                   boundary = random_boundary(), 
+                                   handle = NULL) {
+  if (is.null(config)) config <- config()
+  
+  sep <- paste0("\n--", boundary, "\n")
+  end <- paste0("\n--", boundary, "--\n")
+  
+  body <- paste0(sep, paste0(parts, collapse = sep), end)
+  
+  type <- paste0("multipart/related; boundary=", boundary)
+  config <- c(config, add_headers("Content-Type" = type))
+  
+  POST(url, config = config, body = body, query = list(uploadType = "multipart"), ..., handle = handle)
+}
+
+part <- function(headers, body) {
+  if (length(headers) == 0) {
+    header <- "\n"
+  } else {
+    header <- paste0(names(headers), ": ", headers, "\n", collapse = "")
+  }
+  body <- paste0(body, collapse = "\n")
+  
+  paste0(header, "\n", body)
+}
+
+random_boundary <- function() {
+  valid <- c(LETTERS, letters, 0:9) # , "'", "(", ")", "+", ",", "-", ".", "/", 
+  #  ":", "?")
+  paste0(sample(valid, 50, replace = TRUE), collapse = "")
+}
+
---
+++ R/upload.r
@@ -0,0 +1,81 @@
+#' Upload data.
+#' 
+#' This sends all of the data inline in the HTTP request so is only suitable
+#' for relatively small datasets.
+#' 
+#' @inheritParams insert_query_job 
+#' @param table name of table to insert values into
+#' @param value data frame of data to upload
+#' @seealso Google API documentation: 
+#' \url{https://developers.google.com/bigquery/loading-data-into-bigquery#loaddatapostrequest}
+#' @examples
+#' \dontrun{
+#' list_datasets("193487687779")
+#' list_tables("193487687779", "houston")
+#' job <- insert_upload_job("193487687779", "houston", "mtcars", mtcars)
+#' list_tables("193487687779", "houston")
+#' }
+insert_upload_job <- function(project, dataset, table, values, billing = project) {
+  assert_that(is.string(project), is.string(dataset), is.string(table),
+    is.data.frame(values), is.string(billing))
+  
+  # https://developers.google.com/bigquery/docs/reference/v2/jobs#resource
+  config <- list(
+    configuration = list(
+      load = list(
+        sourceFormat = "CSV",
+        schema = list(
+          fields = schema_fields(values)
+        ),
+        destinationTable = list(
+          projectId = project,
+          datasetId = dataset,
+          tableId = table
+        )
+      )
+    )
+  )
+  config_part <- part(c("Content-type" = "application/json; charset=UTF-8"),
+    toJSON(config, pretty = TRUE))
+
+  csv <- standard_csv(values)
+  csv_part <- part(c("Content-type" = "application/octet-stream"), csv)
+  
+  url <- sprintf("projects/%s/jobs", billing)
+  bq_upload(url, c(config_part, csv_part))
+}
+
+schema_fields <- function(data) {
+  types <- vapply(data, data_type, character(1))
+  Map(function(type, name) list(name = name, type = type), types, names(data))
+}
+
+data_type <- function(x) {
+  switch(class(x)[1], 
+    character = "STRING", 
+    logical = "BOOLEAN",
+    numeric = "FLOAT",
+    integer = "INTEGER",
+    factor = "STRING", 
+    Date = "TIMESTAMP",
+    POSIXct = "TIMESTAMP",
+    stop("Unknown class ", paste0(class(x), collapse = "/"))
+  )
+}
+
+standard_csv <- function(values) {
+  # Convert factors to strings
+  is_factor <- vapply(values, is.factor, logical(1))
+  values[is_factor] <- lapply(values[is_factor], as.character)
+  
+  # Encode special characters in strings
+  is_char <- vapply(values, is.character, logical(1))
+  values[is_char] <- lapply(values[is_char], encodeString)
+  
+  tmp <- tempfile(fileext = ".csv")
+  write.table(values, tmp, sep = ",", quote = FALSE, qmethod = "escape",
+    row.names = FALSE, col.names = FALSE)
+  
+  # Don't read trailing nl
+  readChar(tmp, file.info(tmp)$size - 1, useBytes = TRUE)
+}
