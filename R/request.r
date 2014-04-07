05b334abdc7e89782ce80751140fb4d57cb86069
hadley <h.wickham@gmail.com>
2014-04-07 14:42:05 -0500

    Use new httr features

--- R/request.r
+++ R/request.r
@@ -2,48 +2,29 @@ base_url <- "https://www.googleapis.com/bigquery/v2/"
 upload_url <- "https://www.googleapis.com/upload/bigquery/v2/"
 
 #' @importFrom httr GET config
-bq_get <- function(url, config = NULL, ..., token = get_access_cred()) {
-  if (is.null(config)) {
-    config <- config()
-  }
-  config <- c(config, token = token)
-  req <- GET(paste0(base_url, url), config, ...)
+bq_get <- function(url, ..., token = get_access_cred()) {
+  req <- GET(paste0(base_url, url), config(token = token), ...)
   process_request(req)
 }
 
 #' @importFrom httr DELETE config
-bq_delete <- function(url, config = NULL, ..., token = get_access_cred()) {
-  if (is.null(config)) {
-    config <- config()
-  }
-  config <- c(config, token = token)
-  req <- DELETE(paste0(base_url, url), config, ...)
-  
+bq_delete <- function(url, ..., token = get_access_cred()) {
+  req <- DELETE(paste0(base_url, url), config(token = token), ...)
   process_request(req)
 }
 
 #' @importFrom httr POST add_headers config
-bq_post <- function(url, body, config = NULL, ..., token = get_access_cred()) {
-  if (is.null(config)) {
-    config <- config()
-  }
+bq_post <- function(url, body, ..., token = get_access_cred()) {
   json <- jsonlite::toJSON(body)
-  config <- c(config, token = token, add_headers("Content-type" = "application/json"))
-
-  req <- POST(paste0(base_url, url), config, body = json, ...)
+  req <- POST(paste0(base_url, url), body = json, config(token = token),
+    add_headers("Content-type" = "application/json"), ...)
   process_request(req)
 }
 
 #' @importFrom httr POST add_headers config
-bq_upload <- function(url, parts, config = NULL, ..., token = get_access_cred()) {
-  if (is.null(config)) {
-    config <- config()
-  }
-  
-  config <- c(config, token = token)
-  
+bq_upload <- function(url, parts, ..., token = get_access_cred()) {
   url <- paste0(upload_url, url)
-  req <- POST_multipart_related(url, config, parts = parts, ...)
+  req <- POST_multipart_related(url, parts = parts, config(token = token), ...)
   process_request(req)
 }
 
@@ -52,7 +33,7 @@ bq_upload <- function(url, parts, config = NULL, ..., token = get_access_cred())
 process_request <- function(req) {
   # No content -> success
   if (req$status_code == 204) return(TRUE)
-  
+
   if (http_status(req)$category == "success") {
     return(content(req, "parsed", "application/json"))
   }
@@ -60,15 +41,7 @@ process_request <- function(req) {
   type <- parse_media(req$headers$`Content-type`)
   if (type$complete == "application/json") {
     out <- content(req, "parsed", "application/json")
-    
-    if (out$error$code == 401) {
-      reset_access_cred()
-      stop("Invalid access credentials have been reset. Please try again.",
-        call. = FALSE)
-    } else {
-      stop(out$err$message, call. = FALSE)  
-    }
-    
+    stop(out$err$message, call. = FALSE)
   } else {
     out <- content(req, "text")
     stop("HTTP error [", req$status, "] ", out, call. = FALSE)
@@ -80,19 +53,20 @@ process_request <- function(req) {
 
 # http://www.w3.org/Protocols/rfc1341/7_2_Multipart.html
 POST_multipart_related <- function(url, config = NULL, parts = NULL, ...,
-                                   boundary = random_boundary(), 
+                                   boundary = random_boundary(),
                                    handle = NULL) {
   if (is.null(config)) config <- config()
-  
+
   sep <- paste0("\n--", boundary, "\n")
   end <- paste0("\n--", boundary, "--\n")
-  
+
   body <- paste0(sep, paste0(parts, collapse = sep), end)
-  
+
   type <- paste0("multipart/related; boundary=", boundary)
   config <- c(config, add_headers("Content-Type" = type))
-  
-  POST(url, config = config, body = body, query = list(uploadType = "multipart"), ..., handle = handle)
+
+  POST(url, config = config, body = body,
+    query = list(uploadType = "multipart"), ..., handle = handle)
 }
 
 part <- function(headers, body) {
@@ -102,12 +76,12 @@ part <- function(headers, body) {
     header <- paste0(names(headers), ": ", headers, "\n", collapse = "")
   }
   body <- paste0(body, collapse = "\n")
-  
+
   paste0(header, "\n", body)
 }
 
 random_boundary <- function() {
-  valid <- c(LETTERS, letters, 0:9) # , "'", "(", ")", "+", ",", "-", ".", "/", 
+  valid <- c(LETTERS, letters, 0:9) # , "'", "(", ")", "+", ",", "-", ".", "/",
   #  ":", "?")
   paste0(sample(valid, 50, replace = TRUE), collapse = "")
 }
