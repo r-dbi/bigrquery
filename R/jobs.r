19788ad29ee168f757c692cee6db55c9ae47dece
Janis Kesteris <jkesteris@gmail.com>
2014-10-14 18:41:27 +0200

    test

--- R/jobs.r
+++ R/jobs.r
@@ -43,6 +43,7 @@ insert_query_job <- function(query, project, destination_table = NULL,
       datasetId = destination_table$dataset_id,
       tableId = destination_table$table_id
     )
+    print("TEST")
     #WRITE_TRUNCATE, #WRITE_APPEND, #WRITE_EMPTY
     body$configuration$writeDisposition <- if(is.null(write_disposition)) "WRITE_EMPTY" else write_disposition
   }
