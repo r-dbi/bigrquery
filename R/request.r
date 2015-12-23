8dd1e7800a6444186eb09a7af81b33dacbcb6eaa
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-12-23 10:22:40 +0100

    import status_code

--- R/request.r
+++ R/request.r
@@ -43,7 +43,7 @@ bq_upload <- function(url, parts, ..., query = NULL, token = get_access_cred()) 
 }
 
 
-#' @importFrom httr http_status content parse_media
+#' @importFrom httr http_status content parse_media status_code
 process_request <- function(req) {
   # No content -> success
   if (req$status_code == 204) return(TRUE)
