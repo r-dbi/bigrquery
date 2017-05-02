98482a458dae964ac1ee1c90b46bfe8209e450d8
hadley <h.wickham@gmail.com>
2017-05-02 13:51:44 -0500

    Tweak test

--- tests/testthat/test-sql.R
+++ tests/testthat/test-sql.R
@@ -3,9 +3,9 @@ context("sql")
 test_that("casting uses bigquery types", {
   skip_if_not_installed("dbplyr")
 
-  sql <- dbplyr::lazy_frame(x = "1", src = simulate_bigrquery()) %>%
+  sql <- dbplyr::lazy_frame(x = "1") %>%
     dplyr::mutate(y = as.integer(x), z = as.numeric(x)) %>%
-    dbplyr::sql_build()
+    dbplyr::sql_build(simulate_bigrquery())
 
   expect_equal(sql$select[[2]], 'SAFE_CAST(`x` AS INT64) AS `y`')
   expect_equal(sql$select[[3]], 'SAFE_CAST(`x` AS FLOAT64) AS `z`')
