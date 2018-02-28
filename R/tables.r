9fd1f4d811147e49fdba691cd3d4b98760feff36
byapparov <byapparov@gmail.com>
2018-02-28 22:27:52 +0000

    Add ability to create table with a given schema.

--- R/tables.r
+++ R/tables.r
@@ -32,18 +32,22 @@ list_tables <- function(project, dataset, page_size = 50, max_pages = Inf) {
 #' @export
 #' @seealso API documentation:
 #'  \url{https://developers.google.com/bigquery/docs/reference/v2/tables/insert}
-insert_table <- function(project, dataset, table, ...) {
+insert_table <- function(project, dataset, table, schema = list(), ...) {
   url <- bq_path(project, dataset, "")
+  if (length(schema) > 0) {
+    schema = list(fields = schema)
+  }
   body <- list(
     tableReference = list(
       projectId = project,
       datasetId = dataset,
       tableId = table
-    )
+    ),
+    schema = schema
   )
-
   bq_post(url, body = bq_body(body, ...))
 }
+
 
 #' Retrieve table metadata
 #'
