4a1cd1aef624423fc503ee2c123a0d3328e09b8b
Roland Stevenson <rstevenson@shutterfly.com>
2018-03-06 09:59:43 +0000

    add test for max_pages=0

--- tests/testthat/test-query_exec.R
+++ tests/testthat/test-query_exec.R
@@ -20,3 +20,12 @@ test_that("can convert all date time types", {
   expect_equal(df$date, as.Date(base))
   expect_equal(df$time, hms::hms(hours = 3, minutes = 4, seconds = 5.67))
 })
+
+
+test_that("data is not returned if max_pages is set to zero", {
+  sql <- "SELECT 1 x, 2 y"
+  res <- query_exec(sql, "bigrquery-examples", max_pages = 0)
+  expect_equal(nrow(res), 0, label = "No rows returned")
+  expect_equal(names(res), c("x", "y"), label = "Column names are matching the query")
+  expect_equal(class(res), "data.frame", label = "Class is set to data.frame")
+})
