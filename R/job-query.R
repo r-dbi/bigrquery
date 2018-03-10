e23f0b497f66eb8980aa93740374b93103232888
byapparov <byapparov@gmail.com>
2018-03-10 20:04:34 +0000

    Updated documentation to include new @param.

--- R/job-query.R
+++ R/job-query.R
@@ -22,6 +22,8 @@
 #'   `query`, either as a string in the format used by BigQuery or as a
 #'   list with `project_id` and `dataset_id` entries
 #' @param use_legacy_sql (optional) set to `FALSE` to enable BigQuery's standard SQL.
+#' @param parameters list of named parameters that will be used with parameterised SQL:
+#'   https://cloud.google.com/bigquery/docs/parameterized-queries#bigquery-query-params-api
 #' @family jobs
 #' @return a job resource list, as documented at
 #'   \url{https://developers.google.com/bigquery/docs/reference/v2/jobs}
