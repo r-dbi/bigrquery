1090a940b2900dbf6f7bd75848b993fdc7523116
hadley <h.wickham@gmail.com>
2018-04-03 18:36:54 -0500

    Suppress time zone

--- tests/testthat/test-upload.r
+++ tests/testthat/test-upload.r
@@ -4,9 +4,10 @@ test_that("date/times can be round-tripped", {
   skip_if_no_auth()
 
   df1 <- data.frame(
-    x = Sys.Date(),
-    y = Sys.time()
+    x = as.Date("2018-01-01"),
+    y = as.POSIXct(as.Date("2018-01-01"))
   )
+  attr(df1$y, "tzone") <- "UTC"
   df1$z <- as.POSIXlt(df1$y)
 
   job <- insert_upload_job(bq_test_project(), "test", "x", df1)
@@ -16,3 +17,4 @@ test_that("date/times can be round-tripped", {
   df2 <- list_tabledata(bq_test_project(), "test", "x")
   expect_equal(df1, df2)
 })
+
