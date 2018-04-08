449ce1153ce4dc25933c239107f1fe395d0a3a59
byapparov <byapparov@gmail.com>
2018-04-08 13:40:52 +0100

    Styler update for test-request.r

--- tests/testthat/test-request.r
+++ tests/testthat/test-request.r
@@ -2,11 +2,13 @@ context("request")
 
 test_that("api keys are added when present", {
   tryCatch({
-    key <- 'my.secret.key'
+    key <- "my.secret.key"
     Sys.setenv(BIGRQUERY_API_KEY = key)
     expect_that(bigrquery:::prepare_bq_query(NULL), equals(list(key = key)))
-    expect_that(bigrquery:::prepare_bq_query(list(herring_color = "red")),
-                equals(list(herring_color = "red", key = key)))
+    expect_that(
+      bigrquery:::prepare_bq_query(list(herring_color = "red")),
+      equals(list(herring_color = "red", key = key))
+    )
   }, finally = {
     Sys.unsetenv("BIGRQUERY_API_KEY")
   })
@@ -14,10 +16,12 @@ test_that("api keys are added when present", {
 
 test_that("explicit api keys override env vars", {
   tryCatch({
-    key <- 'my.secret.key'
+    key <- "my.secret.key"
     Sys.setenv(BIGRQUERY_API_KEY = key)
-    expect_that(bigrquery:::prepare_bq_query(list(key = "my.other.key")),
-                equals(list(key = "my.other.key")))
+    expect_that(
+      bigrquery:::prepare_bq_query(list(key = "my.other.key")),
+      equals(list(key = "my.other.key"))
+    )
   }, finally = {
     Sys.unsetenv("BIGRQUERY_API_KEY")
   })
@@ -26,8 +30,10 @@ test_that("explicit api keys override env vars", {
 
 test_that("ellipsis parameter list is added to configuration params", {
   body <- list(configuration = list(
-    query = list(query = "SELECT 1",
-                 useLegacySql = TRUE)
+    query = list(
+      query = "SELECT 1",
+      useLegacySql = TRUE
+    )
   ))
   res <- bq_body(body, dry_run = TRUE)
   expect_equal(
@@ -37,13 +43,11 @@ test_that("ellipsis parameter list is added to configuration params", {
 })
 
 test_that("ellipsis parameter list is added to body without configuration", {
-  body <- list(query = "SELECT 1",
-               useLegacySql = TRUE)
+  body <- list(
+    query = "SELECT 1",
+    useLegacySql = TRUE
+  )
 
   res <- bq_body(body, dry_run = TRUE)
-  expect_equal(
-    res$dryRun,
-    TRUE,
-    label = "camel cased attribute is added to the body of the request"
-  )
+  expect_equal(res$dryRun, TRUE)
 })
