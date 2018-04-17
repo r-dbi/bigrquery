f31a376f290fe6fa47c7f7a9331d48f4446a97bf
hadley <h.wickham@gmail.com>
2018-04-17 12:34:57 -0500

    Use random name to avoid clashes

--- tests/testthat/test-zzz-dep-dataset.R
+++ tests/testthat/test-zzz-dep-dataset.R
@@ -1,13 +1,15 @@
 context("dataset.R")
 
 test_that("extra arguments passed onto request", {
-  insert_dataset(bq_test_project(), "test_temp",
+  name <- random_name()
+
+  insert_dataset(bq_test_project(), name,
     friendlyName = "xxx",
     description = "yyy"
   )
-  on.exit(delete_dataset(bq_test_project(), "test_temp"))
+  on.exit(delete_dataset(bq_test_project(), name))
 
-  ds <- get_dataset(bq_test_project(), "test_temp")
+  ds <- get_dataset(bq_test_project(), name)
   expect_equal(ds$friendlyName, "xxx")
   expect_equal(ds$description, "yyy")
 })
