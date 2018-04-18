53c9c20200a304efbb1dd9136dbc2b15b35db70e
hadley <h.wickham@gmail.com>
2018-04-18 13:45:46 -0500

    Improve test coverage of gs-object

--- R/gs-object.R
+++ R/gs-object.R
@@ -12,17 +12,23 @@ as.character.gs_object <- function(x, ...) {
 
 #' @export
 format.gs_object <- function(x, ...) {
-  glue::glue_data(x, "gs://{bucket}/{object}")
+  as.character(glue::glue_data(x, "gs://{bucket}/{object}"))
 }
 
 #' @export
 print.gs_object <- function(x, ...) {
-  cat_line(format(x))
+  cat_line("<gs_object> ", format(x))
   invisible(x)
 }
 
 gs_object_delete <- function(x, token = get_access_cred()) {
   url <- glue::glue_data(x, "https://www.googleapis.com/storage/v1/b/{bucket}/o/{object}")
-  req <- DELETE(url, config(token = token), httr::user_agent(bq_ua()))
+  req <- httr::DELETE(url, config(token = token), httr::user_agent(bq_ua()))
   process_request(req)
+}
+
+gs_object_exists <- function(x, token = get_access_cred()) {
+  url <- glue::glue_data(x, "https://www.googleapis.com/storage/v1/b/{bucket}/o/{object}")
+  req <- httr::GET(url, config(token = token), httr::user_agent(bq_ua()))
+  req$status_code != 404
 }
