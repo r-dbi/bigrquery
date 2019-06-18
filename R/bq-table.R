r-dbi/bigrquery

commit a7b64c2bc5513065c90c91c5e0957c162b77845b

Updated examples and test to demonstrate how metadata can be added to the table when it is created. (#296)

R/bq-table.R
@@ -25,7 +25,13 @@
 #' if (bq_testable()) {
 #' ds <- bq_test_dataset()
 #'
-#' bq_mtcars <- bq_table(ds, "mtcars")
+#' bq_mtcars <- bq_table(
+#'   ds,
+#'   "mtcars",
+#'   friendly_name = "Motor Trend Car Road Tests",
+#'   description = "The data was extracted from the 1974 Motor Trend US magazine",
+#'   labels = list(category = "example")
+#' )
 #' bq_table_exists(bq_mtcars)
 #'
 #' bq_table_upload(bq_mtcars, mtcars)

man/api-table.Rd
@@ -100,7 +100,13 @@ for uploading and downloading data in to/from memory (\code{bq_table_upload()},
 if (bq_testable()) {
 ds <- bq_test_dataset()
 
-bq_mtcars <- bq_table(ds, "mtcars")
+bq_mtcars <- bq_table(
+  ds,
+  "mtcars",
+  friendly_name = "Motor Trend Car Road Tests",
+  description = "The data was extracted from the 1974 Motor Trend US magazine",
+  labels = list(category = "example")
+)
 bq_table_exists(bq_mtcars)
 
 bq_table_upload(bq_mtcars, mtcars)

tests/testthat/test-bq-table.R
@@ -6,7 +6,13 @@ test_that("can create and delete tables", {
   bq_mtcars <- bq_table(ds, "mtcars")
   expect_false(bq_table_exists(bq_mtcars))
 
-  bq_table_create(bq_mtcars, mtcars)
+  bq_table_create(
+    bq_mtcars,
+    mtcars,
+    friendly_name = "Motor Trend Car Road Tests",
+    description = "The data was extracted from the 1974 Motor Trend US magazine",
+    labels = list(category = "test")
+  )
   expect_true(bq_table_exists(bq_mtcars))
 
   bq_table_delete(bq_mtcars)

commit e9ea8e4ba99244bebd4504a6a1453873f479e667

Fixing example with wrong function
Example introduced in #296 referenced the wrong function

R/bq-table.R
@@ -25,13 +25,14 @@
 #' if (bq_testable()) {
 #' ds <- bq_test_dataset()
 #'
-#' bq_mtcars <- bq_table(
+#' bq_mtcars <- bq_table_create(
 #'   ds,
 #'   "mtcars",
 #'   friendly_name = "Motor Trend Car Road Tests",
 #'   description = "The data was extracted from the 1974 Motor Trend US magazine",
 #'   labels = list(category = "example")
 #' )
+#' bq_mtcars <- bq_table(ds, "mtcars")
 #' bq_table_exists(bq_mtcars)
 #'
 #' bq_table_upload(bq_mtcars, mtcars)