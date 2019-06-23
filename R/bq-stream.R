2ae7cb807fb942421863014f3b9aaeaf38316aab
byapparov <byapparov@gmail.com>
2019-06-23 17:41:01 +0100

    * Added option to inject insert_ids (row id for de-duplication in the stream)
    * Added test for wrong schema writing to check that list of errors is returned

--- R/bq-stream.R
+++ R/bq-stream.R
@@ -2,6 +2,9 @@
 #'
 #' @param x bq_table where values will be streamed to
 #' @param values data.frame that will be streamed
+#' @param insert_ids
+#'   (optional) Unique ids, one per row being inserted.
+#'   If omitted, unique IDs are created.
 #' @param skip_invalid_rows
 #'   (optional) Insert all valid rows of a request,
 #'   even if invalid rows exist. The default value is False, which
@@ -15,20 +18,28 @@
 #'   BigQuery will create the table `<name> + <template_suffix>` based
 #'   on the schema of the template table. See
 #'   https://cloud.google.com/bigquery/streaming-data-into-bigquery#template-tables
+#' @return NULL if there are no errors, otherwise list of errors with reasons
 #' @export
 #' @rdname bq-streaming
 #' @seealso https://cloud.google.com/bigquery/streaming-data-into-bigquery
-bq_table_stream <- function(x, values, skip_invalid_rows = "false", ignore_unknown_values = "false", template_suffix = NULL) {
+bq_table_stream <- function(x,
+                            values,
+                            insert_ids = bq_stream_random_id(nrow(values)),
+                            skip_invalid_rows = "false",
+                            ignore_unknown_values = "false",
+                            template_suffix = NULL) {
   x <- as_bq_table(x)
   url <- bq_path(x$project, x$dataset, x$table)
   url <- paste0(url, "/", "insertAll")
   body <- bq_stream_body(
     x = values,
+    insert_ids = insert_ids,
     skip_invalid_rows = skip_invalid_rows,
     ignore_unknown_values = ignore_unknown_values,
     template_suffix = template_suffix
   )
-  bq_post(url, body)
+  response <- bq_post(url, body)
+  invisible(response$insertErrors)
 }
 
 #' Converts data.table to streaming request body
@@ -36,15 +47,21 @@ bq_table_stream <- function(x, values, skip_invalid_rows = "false", ignore_unkno
 #' @noRd
 #' @return list that represents json for streaming
 #' @seealso https://cloud.google.com/bigquery/docs/reference/rest/v2/tabledata/insertAll#InsertionRow
-bq_stream_body <- function(x, skip_invalid_rows = "false", ignore_unknown_values = "false", template_suffix = NULL) {
-  l <- setNames(split(x, seq(nrow(x))), rownames(x))
-  rows <- lapply(l, function(row) {
+bq_stream_body <- function(x,
+                           insert_ids,
+                           skip_invalid_rows,
+                           ignore_unknown_values,
+                           template_suffix
+                           ) {
+
+
+  rows <- lapply(1:nrow(x), function(row.index) {
     list(
-      insertId = paste(sample(c(letters[1:6], 0:9), 30, replace = TRUE), collapse = ""),
-      json = as.list(row)
+      insertId = insert_ids[row.index],
+      json = as.list(x[row.index, ])
     )
   })
-  names(rows) <- NULL
+
   body <- list(
     kind = "bigquery#tableDataInsertAllRequest",
     skipInvalidRows = skip_invalid_rows,
@@ -56,4 +73,16 @@ bq_stream_body <- function(x, skip_invalid_rows = "false", ignore_unknown_values
     body["templateSuffix"] <- template_suffix
   }
   body
+}
+
+#' Generates random string that BigQuery uses do de-duplicate records
+#'
+#' @noRd
+bq_stream_random_id <- function(n = 1) {
+ assert_that(n > 0)
+ vapply(
+   1:n,
+   FUN = function(x) paste(sample(c(letters[1:6], 0:9), 30, replace = TRUE), collapse = ""),
+   FUN.VALUE = character(1)
+  )
 }
