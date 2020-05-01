500f56e3a189a866700c9eb7029d2c6c33c6229e
Hadley Wickham <h.wickham@gmail.com>
2020-05-01 16:38:15 -0500

    Print some token info

--- tests/testthat/helper-auth.R
+++ tests/testthat/helper-auth.R
@@ -1,7 +1,10 @@
 print(!identical(Sys.getenv("BIGRQUERY_PASSWORD"), ""))
 print(gargle:::secret_can_decrypt("bigrquery"))
 
+
 if (gargle:::secret_can_decrypt("bigrquery")) {
   json <- gargle:::secret_read("bigrquery", "bigrquery-testing.json")
   bq_auth(path = rawToChar(json))
+  print(.auth$cred)
+  print(class(.auth$cred))
 }
