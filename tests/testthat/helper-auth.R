ffa8a1fc679816cf8d1172ca312f8cace0fe027b
Hadley Wickham <h.wickham@gmail.com>
2020-05-01 16:26:02 -0500

    Add some debugging info

--- tests/testthat/helper-auth.R
+++ tests/testthat/helper-auth.R
@@ -1,3 +1,6 @@
+print(!identical(Sys.getenv("BIGRQUERY_PASSWORD"), ""))
+print(gargle:::secret_can_decrypt("bigrquery"))
+
 if (gargle:::secret_can_decrypt("bigrquery")) {
   json <- gargle:::secret_read("bigrquery", "bigrquery-testing.json")
   bq_auth(path = rawToChar(json))
