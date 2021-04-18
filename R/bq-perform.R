9dc98018b4ef3f08ff1297a2ce3449c3b9efd7fc
byapparov <byapparov@gmail.com>
2021-04-18 22:58:19 +0100

    Reduce and correct the if statement

--- R/bq-perform.R
+++ R/bq-perform.R
@@ -263,7 +263,7 @@ bq_perform_query <- function(query, billing,
     priority = unbox(priority)
   )
 
-  if (!is.null(parameters) & !(length(parameters) == 0)) {
+  if (!is.null(parameters) && length(parameters) > 0) {
     parameters <- as_bq_params(parameters)
     query$queryParameters <- as_json(parameters)
   }
