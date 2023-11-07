a1d3f528ed5f343dda79d90eaf639242dafdb593
Hadley Wickham <h.wickham@gmail.com>
2023-11-07 16:55:24 -0600

    Use modern gargle secret management

--- tests/testthat/helper-auth.R
+++ tests/testthat/helper-auth.R
@@ -1,4 +1,5 @@
-if (gargle:::secret_can_decrypt("bigrquery")) {
-  json <- gargle:::secret_read("bigrquery", "bigrquery-testing.json")
-  bq_auth(path = rawToChar(json))
+if (gargle::secret_has_key("BIGRQUERY_KEY")) {
+  path <- system.file("secret", "bigrquery-testing.json", package = "bigrquery")
+  json <- gargle::secret_decrypt_json(path, "BIGRQUERY_KEY")
+  bq_auth(path = json)
 }
