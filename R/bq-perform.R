60a5d2f343156413d4821201a722e4f8287a3a2f
apalacio9502 <alejandro.palacio@outlook.com>
2024-06-08 13:34:44 -0500

    Drop export_parquet wrapper function

--- R/bq-perform.R
+++ R/bq-perform.R
@@ -157,9 +157,10 @@ bq_perform_upload <- function(x, values,
       "content" = export_json(values)
     )
   } else {
+    # https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-parquet?hl=es-419
     media <- list(
       "type" = "application/vnd.apache.parquet",
-      "content" = export_parquet(values)
+      "content" = nanoparquet::write_parquet(values, ":raw:")
     )
   }
 
@@ -197,13 +198,6 @@ export_json <- function(values) {
   jsonlite::stream_out(values, con, verbose = FALSE, na = "null")
 
   rawToChar(rawConnectionValue(con))
-}
-
-# https://cloud.google.com/bigquery/docs/loading-data-cloud-storage-parquet?hl=es-419
-export_parquet <- function(values) {
-
-  nanoparquet::write_parquet(values, ":raw:")
-
 }
 
 #' @export
