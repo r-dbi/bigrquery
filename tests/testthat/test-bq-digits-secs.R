ab4a0497190447af4adca0b744dc10fdc32b3c18
Bill Evans <bill@8pawexpress.com>
2026-01-16 09:47:49 -0500

    tweak tests

--- tests/testthat/test-bq-digits-secs.R
+++ tests/testthat/test-bq-digits-secs.R
@@ -1,7 +1,6 @@
-tbl <- data.frame(psx = trunc.Date(Sys.time(), units = "secs") + 0.123456)
-attr(tbl$psx, "tzone") <- "America/New_York"
-
 test_that("'POSIXt' preserves microsecond precision", {
+  tbl <- data.frame(psx = trunc.Date(Sys.time(), units = "secs") + 0.123456)
+  attr(tbl$psx, "tzone") <- "UTC"
 
   ds <- bq_table(bq_test_project(), "basedata", "datatypes")
   defer(try(bq_table_delete(ds), silent = TRUE))
@@ -9,12 +8,10 @@ test_that("'POSIXt' preserves microsecond precision", {
   bq_table_upload(ds, tbl)
   tbl2 <- bq_table_download(ds)
 
-  expect_true(abs(as.numeric(tbl2$psx - tbl$psx, units = "secs")) < 1e-6)
-
+  expect_equal(tbl$psx, tbl2$psx)
 })
 
 test_that("'tzone' unset and different values handled correctly", {
-
   # the bug only produced problems when the system TZ has a non-zero
   # offset (i.e., not UTC)
   withr::local_envvar(TZ = "America/New_York")
@@ -22,22 +19,27 @@ test_that("'tzone' unset and different values handled correctly", {
   ds <- bq_table(bq_test_project(), "basedata", "datatypes")
   defer(try(bq_table_delete(ds), silent = TRUE))
 
-
-  bq_table_upload(ds, transform(tbl, psx = `attr<-`(psx, "tzone", NULL)))
-  tbl5 <- bq_table_download(ds)
+  tbl1in <- tibble(psx = trunc.Date(Sys.time(), units = "secs") + 0.123456)
+  attr(tbl1in$psx, "tzone") <- NULL
+  bq_table_upload(ds, tbl1in)
+  tbl1out <- bq_table_download(ds)
   bq_table_delete(ds)
+  attr(tbl1out$psx, "tzone") <- NULL
+  expect_equal(tbl1in, tbl1out)
   expect_true(abs(as.numeric(tbl5$psx - tbl$psx, units = "hours")) < 1)
 
-
-  bq_table_upload(ds, transform(tbl, psx = `attr<-`(psx, "tzone", "UTC")))
-  tbl6 <- bq_table_download(ds)
+  tbl2in <- tibble(psx = trunc.Date(Sys.time(), units = "secs") + 0.123456)
+  attr(tbl2in$psx, "tzone") <- "UTC"
+  bq_table_upload(ds, tbl2in)
+  tbl2out <- bq_table_download(ds)
   bq_table_delete(ds)
-  expect_true(abs(as.numeric(tbl6$psx - tbl$psx, units = "hours")) < 1)
+  expect_equal(tbl2in, tbl2out)
 
-
-  bq_table_upload(ds, transform(tbl, psx = `attr<-`(psx, "tzone", "America/New_York")))
-  tbl7 <- bq_table_download(ds)
+  tbl3in <- tibble(psx = trunc.Date(Sys.time(), units = "secs") + 0.123456)
+  attr(tbl3in$psx, "tzone") <- "America/New_York"
+  bq_table_upload(ds, tbl3in)
+  tbl3out <- bq_table_download(ds)
   bq_table_delete(ds)
-  expect_true(abs(as.numeric(tbl7$psx - tbl$psx, units = "hours")) < 1)
-
+  attr(tbl3out$psx, "tzone") <- attr(tbl3in$psx, "tzone")
+  expect_equal(tbl3in, tbl3out)
 })
