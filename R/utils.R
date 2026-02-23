6e6b2476bbe4236d40358d7a109a5f2ef23770b8
JulianUmbhau <julian@umbhau.dk>
2026-02-23 14:30:38 +0100

    adding labels to calls, check_labels function, default NULL for labels and tests for check_labels function

--- R/utils.R
+++ R/utils.R
@@ -91,3 +91,62 @@ cli_escape <- function(x) {
   x <- gsub("}", "}}", x, fixed = TRUE)
   x
 }
+
+check_labels <- function(labels) {
+  # Handle NULL, NA, or empty inputs
+  if (
+    is.null(labels) ||
+      length(labels) == 0 ||
+      (length(labels) == 1 && is.na(labels))
+  ) {
+    return(NULL)
+  }
+
+  if (!is.list(labels)) {
+    warning(
+      paste0("Labels must to be a dictionary list; dropping labels"),
+      immediate. = TRUE
+    )
+    return(NULL)
+  }
+  nms <- names(labels)
+  if (is.null(nms) || anyNA(nms) || any(nms == "")) {
+    warning(
+      "Label keys must be non-empty strings; dropping labels",
+      immediate. = TRUE,
+      call. = FALSE
+    )
+    return(NULL)
+  }
+  for (nm in names(labels)) {
+    if (!is.character(labels[[nm]]) || length(labels[[nm]]) != 1) {
+      warning(
+        sprintf("Label '%s' must be a single string; dropping labels", nm),
+        immediate. = TRUE
+      )
+      return(NULL)
+    }
+    if (nm != tolower(nm)) {
+      warning(
+        sprintf(
+          "Label key '%s' must match ^[a-z0-9_-]{0,62}$; dropping labels",
+          nm
+        ),
+        immediate. = TRUE
+      )
+      return(NULL)
+    }
+    if (labels[[nm]] != tolower(labels[[nm]])) {
+      warning(
+        sprintf(
+          "Label value '%s' must be empty or match ^[a-z0-9_-]{0,62}$; dropping labels",
+          labels[[nm]]
+        ),
+        immediate. = TRUE
+      )
+      return(NULL)
+    }
+  }
+
+  return(labels)
+}
